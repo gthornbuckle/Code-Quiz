fcc4810f7473b3e9db41b03a2666a3be5f2c32b0
gthornbuckle <gthornbuckle@gmail.com>
2023-01-17 17:53:23 +0000

    Adds functionality to check whether a selected answer is correct

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,3 +1,6 @@
+var currentQuestion = 0;
+var timeLeft = 0;
+
 function quizTimer() {
     var timeLeft = 60;
     var timeInterval = setInterval(function () {
@@ -17,6 +20,17 @@ function outOfTime(){
     console.log("Out of time.");
 }
 
+function checkAnswer(event){
+    var element = event.target;
+    var selectedAnswer = element.getAttribute("data-id"); //Takes id of chosen button
+    if(questions[currentQuestion].answers[selectedAnswer][1] === true){ //Checks answer array to confirm if selected answer is correct
+        console.log("Correct answer selected");
+    }
+    else{
+        console.log("Incorrect answer selected");
+    }
+}
+
 function startQuiz(){
     console.log("Quiz started.");
     quizTimer(); //Starts quiz timer
@@ -24,17 +38,19 @@ function startQuiz(){
     document.getElementById('start-screen').className = 'hide'; //Hides start screen
     document.getElementById('questions').className ='start'; //Displays question screen
 
+    document.getElementById('question-title').textContent = questions[currentQuestion].question; //Displays question on page
+    document.getElementById('choice1').textContent = questions[currentQuestion].answers[0][0]; //Displays answers within 4 buttons on page
+    document.getElementById('choice2').textContent = questions[currentQuestion].answers[1][0];
+    document.getElementById('choice3').textContent = questions[currentQuestion].answers[2][0];
+    document.getElementById('choice4').textContent = questions[currentQuestion].answers[3][0];
     
-    document.getElementById('question-title').textContent = questions[0].question;
-    document.getElementById('choiceA').textContent = questions[0].answers[0];
-    document.getElementById('choiceB').textContent = questions[0].answers[1];
-    document.getElementById('choiceC').textContent = questions[0].answers[2];
-    document.getElementById('choiceD').textContent = questions[0].answers[3];
-    // for (var i = 0; i < questions.length; i++){
-
+    document.querySelector('#choice1').addEventListener('click', checkAnswer);
+    document.querySelector('#choice2').addEventListener('click', checkAnswer);
+    document.querySelector('#choice3').addEventListener('click', checkAnswer);
+    document.querySelector('#choice4').addEventListener('click', checkAnswer);
 
 };
 
 
-var startButton = document.querySelector('#start');
-startButton.addEventListener('click', startQuiz);
+
+document.querySelector('#start').addEventListener('click', startQuiz);
--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,6 +1,26 @@
 var questions = [
     {
-    question: "Which of these is not a data type?",
-    answers: ["String", "Boolean", "Integer", "Button"]
-},
+        question: "Which of these is not a data type?",
+        answers: [["String", false], ["Boolean", false], ["Integer", false], ["Button", true]]
+    },
+    {
+        question: "Elements within a Javascript array must be enclosed by which symbols?",
+        answers: ["Parentheses ( )", "Square Brackets [ ]", "Curly Brackets { }", "Angle Brackets < >"]
+    },
+    {
+        question: "Javascript Arrays can store what?",
+        answers: ["Strings", "Other Arrays", "Objects", "All of the Above"]
+    },
+    {
+        question: "In order to output variable 'x' to the browser developer console you would use what?",
+        answers: ["console.log(x)", "console.print(x)", "return(x)", "print(x)"]
+    },
+    {
+        question: "In order to output variable 'x' to the browser developer console you would use what?",
+        answers: ["console.log(x)", "console.print(x)", "return(x)", "print(x)"]
+    },
+    {
+        question: "Which of these Javascript operators means equal in both value and type?",
+        answers: ["=", "!=", "===", "=="]
+    }
 ]
