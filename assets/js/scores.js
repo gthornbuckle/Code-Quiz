f923fb9991a2037a72e7fff9059aab672e33e17b
gthornbuckle <gthornbuckle@gmail.com>
2023-01-17 20:39:15 +0000

    Adds ability to clear highscores with confirmation prompt

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -1,16 +1,30 @@
-var highscores = JSON.parse(localStorage.getItem("highscores"));;
+var highscores = JSON.parse(localStorage.getItem("highscores")); //Retrieves highscore array from local storage
 var highscoreList = document.querySelector("#highscores");
 
 function displayHighscores(){
 
-    html = "";
+    scoreDisplay = "";
 
     for(var i = 0; i < highscores.length; i++){
 
-        html += "<li>" +highscores[i]+ "</li>";
+        scoreDisplay += "<li>" +highscores[i][0]+ "  -  " +highscores[i][1]+ "</li>"; //Creates a new list item for each highscore
     }
 
-    document.getElementById("highscores").innerHTML = html;
+    document.getElementById("highscores").innerHTML = scoreDisplay;
 }
 
+function clearHighscores(){
+   var removeScoreConfirmation = confirm("Are you sure you want to clear the highscores?");
+   if(removeScoreConfirmation === true){
+
+        highscores = []; //Sets highscores array to empty and saves to local storage
+        localStorage.setItem("highscores", JSON.stringify(highscores));
+        displayHighscores();
+   }
+   else{
+    return;
+   }
+}
+
+document.querySelector('#clear').addEventListener('click', clearHighscores);
 displayHighscores();
